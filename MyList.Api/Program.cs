f3fb4de1bc7b64fc121983496284ec97bb04d50b
jebef <wjebef@berkeley.edu>
2026-02-15 13:44:13 -0800

    Implemented basic API endpoints for communicating with DB

--- MyList.Api/Program.cs
+++ MyList.Api/Program.cs
@@ -1,13 +1,14 @@
+using System.Data;
+using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 using MyList.Api.Data;
+using MyList.Shared.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//--- DI ---//
 builder.Services.AddOpenApi();
 
-//--- Database ---//
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -21,28 +22,53 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+// These request handlers use DI in their method parameters. The framework 
+// will check the DI container, retrieve the dependency, and pass it on to 
+// the handler. 
+//
+// (AppDbContext db)
+
+/* 
+    Create shows
+*/
+app.MapPost("/api/shows", async (List<Show> shows, AppDbContext db) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    db.Shows.AddRange(shows);
+    await db.SaveChangesAsync();
+    return Results.Created("api/shows", shows);
+});
+
+/* 
+    Delete a show by ID
+*/
+app.MapDelete("api/shows/{id}", async (int id, AppDbContext db) =>
+{
+    var show = await db.Shows.FindAsync(id);
+    if (show is null) return Results.NotFound();
+
+    db.Shows.Remove(show);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+/* 
+    Get all shows 
+*/
+app.MapGet("/api/shows", async (AppDbContext db) =>
+{
+   var shows = await db.Shows.ToListAsync();
+   return Results.Ok(shows);
+});
+
+/* 
+    Get a show by ID 
+*/
+app.MapGet("/api/shows/{id}", async (int id, AppDbContext db)=>
+{
+   var show = await db.Shows.FindAsync(id);
+   return show is not null ? Results.Ok(show) : Results.NotFound(); 
+});
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
